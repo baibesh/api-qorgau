314b2b36b642b2ea54796e2f8b4cc57a6dd31784
Mukhtarkhan Baibesh <baibesh91@gmail.com>
2025-08-05 01:22:33 +0500

    feat: swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configure Swagger only in development environment
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Qorgau API')
+      .setDescription('The Qorgau API description')
+      .setVersion('1.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/docs', app, document);
+  }
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
